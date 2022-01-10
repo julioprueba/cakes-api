62ee9038540c185579389a1905d44873cc58e683
julioperezdecastrovanadis <julio.perezdecastro@vanadis.es>
2022-01-10 15:39:09 +0100

    apply format with prettier

--- tests/BoundedContext/application/CakeCreator.test.ts
+++ tests/BoundedContext/application/CakeCreator.test.ts
@@ -3,21 +3,20 @@ import { CakeRepository } from '../../../src/BoundedContext/Cakes/domain/CakeRep
 import { CakeToCreate } from '../../../src/BoundedContext/Cakes/domain/CakeToCreate';
 
 describe('CakeCreator', () => {
-  it('should create a valid cake', async () => {
-    const repository: CakeRepository = {
+    it('should create a valid cake', async () => {
+        const repository: CakeRepository = {
             find: jest.fn(),
-      save: jest.fn(),
-      update: jest.fn(),
-      delete: jest.fn()
+            save: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn()
+        };
+        const creator = new CakeCreator(repository);
+        const name = 'name';
+        const img = 'img';
+        const expectedCake = new CakeToCreate(name, img);
 
-    };
-    const creator = new CakeCreator(repository);
-    const name = 'name';
-    const img = 'img';
-    const expectedCake = new CakeToCreate(name, img);
+        await creator.run(name, img);
 
-    await creator.run(name, img);
-
-    expect(repository.save).toHaveBeenCalledWith(expectedCake);
-  });
+        expect(repository.save).toHaveBeenCalledWith(expectedCake);
+    });
 });
