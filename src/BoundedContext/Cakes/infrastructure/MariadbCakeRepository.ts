30ee12287233f98892130c3aff53bdd5503f29fd
julioperezdecastrovanadis <julio.perezdecastro@vanadis.es>
2022-01-10 20:36:22 +0100

    implement MariadbCakeRepository and update dependencies

---
+++ src/BoundedContext/Cakes/infrastructure/MariadbCakeRepository.ts
@@ -0,0 +1,42 @@
+import { Cake } from '../domain/Cake';
+import { CakeToCreate } from '../domain/CakeToCreate';
+import { CakeRepository } from '../domain/CakeRepository';
+
+export class MariadbCakeRepository implements CakeRepository {
+    private pool;
+
+    constructor() {
+        // TODO: extraer a un archivo de configuración
+        const mariadb = require('mariadb');
+        this.pool = mariadb.createPool({
+            host: '127.0.0.1',
+            user: 'root',
+            password: 'root',
+            database: 'cakes'
+        });
+    }
+
+    async save(cakeToCreate: CakeToCreate): Promise<Cake> {
+        // TODO: implementar con ORM
+        const conn = await this.pool.getConnection();
+        const query = await conn.query(`INSERT INTO cakes (name, img) VALUES ('${cakeToCreate.name}', '${cakeToCreate.img}');`);
+        const cake = new Cake(query.insertId, cakeToCreate.name, cakeToCreate.img);
+        conn.end();
+        return cake;
+    }
+
+    async find(id: number): Promise<Cake> {
+        // TODO: implementar
+        throw new Error('Method not implemented.');
+    }
+
+    async update(cake: Cake): Promise<void> {
+        // TODO: implementar
+        throw new Error('Method not implemented.');
+    }
+
+    async delete(id: number): Promise<void> {
+        // TODO: implementar
+        throw new Error('Method not implemented.');
+    }
+}
