a439e9c8dd35fdb74a09c714edfa197a2da1baf8
julioperezdecastrovanadis <julio.perezdecastro@vanadis.es>
2022-01-10 20:35:05 +0100

    refactor CakePostController

--- src/app/controllers/CakePostController.ts
+++ src/app/controllers/CakePostController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from 'express';
 import { Controller } from './Controller';
+import { CakeCreator } from '../../BoundedContext/Cakes/application/CakeCreator';
+
+type CakePutRequest = Request & {
+    body: {
+        name: string;
+        img: string;
+    };
+};
 
 export class CakePostController implements Controller {
-    constructor() {}
+    constructor(private cakeCreator: CakeCreator) {}
 
-    async run(req: Request, res: Response) {
-        res.status(201).send();
+    async run(req: CakePutRequest, res: Response) {
+        try {
+            const cake = await this.cakeCreator.run(req.body.name, req.body.img);
+            res.status(201).send(cake);
+        } catch (error) {
+            res.status(500).send(error);
+        }
     }
 }
